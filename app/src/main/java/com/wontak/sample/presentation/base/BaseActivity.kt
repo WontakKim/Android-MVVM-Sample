9f378eb79a9bee5e3f3196e9ac0423dd4b5732eb
Wontak Kim <wontak0103@gmail.com>
2018-11-07 00:23:26 -0500

    Put @LayoutRes annotation

--- app/src/main/java/com/wontak/sample/presentation/base/BaseActivity.kt
+++ app/src/main/java/com/wontak/sample/presentation/base/BaseActivity.kt
@@ -1,11 +1,13 @@
 package com.wontak.sample.presentation.base
 
 import android.os.Bundle
+import android.support.annotation.LayoutRes
 import butterknife.ButterKnife
 import dagger.android.support.DaggerAppCompatActivity
 
 abstract class BaseActivity : DaggerAppCompatActivity() {
 
+    @LayoutRes
     protected abstract fun getLayoutRes(): Int
 
     override fun onCreate(savedInstanceState: Bundle?) {
