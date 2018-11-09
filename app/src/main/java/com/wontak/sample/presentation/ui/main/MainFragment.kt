6883de289f9bca55d00aa2ce3cf877dffb0977f9
Wontak Kim <wontak0103@gmail.com>
2018-11-08 22:05:01 -0500

    Add uis and click listener

--- app/src/main/java/com/wontak/sample/presentation/ui/main/MainFragment.kt
+++ app/src/main/java/com/wontak/sample/presentation/ui/main/MainFragment.kt
@@ -2,20 +2,13 @@ package com.wontak.sample.presentation.ui.main
 
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
-import butterknife.BindView
 import com.wontak.sample.R
 import com.wontak.sample.presentation.base.BaseFragment
 import com.wontak.sample.presentation.di.ViewModelFactory
 import javax.inject.Inject
 
 class MainFragment : BaseFragment() {
-
-    @BindView(R.id.label_name)
-    lateinit var textLabel: TextView
 
     @Inject
     lateinit var viewModelFactory: ViewModelFactory
--- app/src/main/java/com/wontak/sample/presentation/ui/main/MainViewModel.kt
+++ app/src/main/java/com/wontak/sample/presentation/ui/main/MainViewModel.kt
@@ -1,5 +1,6 @@
 package com.wontak.sample.presentation.ui.main
 
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import android.os.Bundle
 import javax.inject.Inject
@@ -7,8 +8,11 @@ import javax.inject.Inject
 class MainViewModel
 @Inject constructor() : ViewModel() {
 
-    fun restoreInstanceState(savedInstanceState: Bundle?) {
+    val counter = MutableLiveData<Int>()
 
-    }
+    var number = 0
 
+    fun restoreInstanceState(savedInstanceState: Bundle?) {}
+
+    fun onButtonClick() = counter.setValue(++number)
 }
